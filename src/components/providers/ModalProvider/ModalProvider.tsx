8548a2b57f92e648bd376cdf9b2b8958f9e3615d
Timotej Cerar <timi.cerar@gmail.com>
2026-09-24 00:43:15 +0200

    feat: add logic to close all modals if route changes

--- src/components/providers/ModalProvider/ModalProvider.tsx
+++ src/components/providers/ModalProvider/ModalProvider.tsx
@@ -1,7 +1,23 @@
+import { useLocation } from '@solidjs/router';
+import { createEffect, on } from 'solid-js';
 import ConfirmationModal from '~/components/compositions/Modals/ConfirmationModal/ConfirmationModal';
 import PokemonDetailsModal from '~/components/compositions/Modals/PokemonDetailsModal/PokemonDetailsModal';
+import { ModalTypes } from '~/constants/modal-provider';
+import { closeModal } from '~/store/modals';
 
 const ModalProvider = () => {
+  const location = useLocation();
+
+  const closeAllModals = () => {
+    for (const type of Object.values(ModalTypes)) {
+      closeModal(type);
+    }
+  };
+
+  createEffect(
+    on(() => location.pathname, closeAllModals, { defer: true }),
+  );
+
   return (
     <>
       <ConfirmationModal />
